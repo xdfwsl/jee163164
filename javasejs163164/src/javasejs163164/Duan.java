0ba574b18a510152ccb332d35735b4d809868c2b
HongyanLi <admin@5uu.net>
2016-11-03 17:03:47 +0800

    03

---
+++ javasejs163164/src/javasejs163164/Duan.java
@@ -0,0 +1,17 @@
+package javasejs163164;
+
+public class Duan {
+
+	// 我想打印5次,结果打印了4次
+	public static void main(String[] args) {
+		int i = 1;
+		do {
+
+			System.out.println("你好XPC学员!");
+			i = i + 1;
+
+		} while (i < 5);
+
+	}
+
+}
---
+++ javasejs163164/src/javasejs163164/Fc.java
@@ -0,0 +1,58 @@
+package javasejs163164;
+
+public class Fc {
+
+	public static void main(String[] args) {
+
+		/*
+				double f; // 华氏
+				double c = 0; // 摄氏
+				int count = 1; // 条目
+				System.out.println("摄氏温度" + "\t华氏温度");
+				do {
+					f = c * 9 / 5.0 + 32; // 转换
+					System.out.println(c + "\t" + f);
+					c = c + 20;
+					count++;
+				} while (count <= 10 && c <= 250);*/
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		double  c=0;//摄氏
+		double f=0;//华氏
+		int count=0;
+		System.out.println("摄氏\t华氏");
+		do {
+			f=c*9/5+32;
+			System.out.println(c+"\t"+f);
+			c=c+20;
+			count++;
+			
+		} while (count<10&&c<=250);
+		
+			}
+		
+
+	}
+
